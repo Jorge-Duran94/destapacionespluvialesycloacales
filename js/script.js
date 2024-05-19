a5ecac95b54f2c332d53b64111519cea40639186
Jorge Duran <129323408+Jorge-Duran94@users.noreply.github.com>
2024-05-19 02:31:52 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-function seleccionar(link) {
-    var opciones = document.querySelectorAll(".nav__link--menu a");
-    opciones.forEach(opcion => opcion.classList.remove("seleccionado"));
-    link.classList.add("seleccionado");
-
-    // Ocultar el menú después de seleccionar una opción en el modo responsive
-    var x = document.querySelector(".nav__menu");
-    x.style.display = "none";
-}
-
-
 
 document.addEventListener("DOMContentLoaded", function() {
     var copy = document.querySelector(".logos-slide").cloneNode(true);
